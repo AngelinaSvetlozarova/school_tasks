1c3e755d96452041cb5730ab3060e82f44df9389
AngelinaSvetlozarova <127393773+AngelinaSvetlozarova@users.noreply.github.com>
2024-02-20 23:56:39 +0200

    Update two_double_variables.c

--- pointers_exercise/two_double_variables.c
+++ pointers_exercise/two_double_variables.c
@@ -2,7 +2,11 @@
 
 int main()
 {
-  double a = 10, b = 15;
+  double a, b;
+  printf("a=");
+  scanf("%lf", &a);
+  printf("\nb=");
+  scanf("%lf", &b);
   double *pa = &a, *pb = &b;
   printf("A: adress:%p value:%lf", pa, *pa);
   printf("\nB: adress:%p value:%lf", pb, *pb);
